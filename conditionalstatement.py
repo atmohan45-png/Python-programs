13ad8664e9e3d3b39a379a41e904a2e27d54f0d7
unknown <atmohan45@gmail.com>
2026-02-12 14:26:02 +0530

    new

---
+++ conditionalstatement.py
@@ -0,0 +1,18 @@
+n=10
+if(n%2==0):
+    print(n,"is an even number")
+else:    
+    print(n,"is an odd number")
+    
+n=int(input("enter a mark : "))
+if(n>=90):
+    print("grade is A")     
+elif(n>=80):
+    print("grade is B") 
+elif(n>=70):
+    print("grade is C")
+elif(n>=60):
+    print("grade is D")
+else:
+    print("grade is F")
+    
---
+++ for loop.py
@@ -0,0 +1,23 @@
+""" for i in range(5):
+    print(i) 
+for i in range(10):
+    if i%2==0:
+        print(i)
+        
+sum=0
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+        sum+=i
+print("sum of even numbers from 1 to 10 is",sum)
+
+n=int(input("enter a number : "))
+for i in range(1,11):
+    print(n,"x",i,"=",n*i)
+    
+star="*"
+i=1 
+while(i<=5):
+    print(i*star)
+    i+=1
+    """
